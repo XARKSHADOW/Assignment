1886caa8ae394c1044b42989714be2b30461687e
Abdul-Halim Bello <alphaascorpio@proton.me>
2023-03-28 13:16:54 +0000

    Assignmet submit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// use the while loop to impliment mul of 5
+// use the do  while loop for the same 
+// for loop to print all even numbers
+$num=1;
+
+while($num<=12){
+    echo "5" . " * " .  $num  ." = " .  5 * $num ."<br>";
+    $num++;
+}
---
+++ multiplication_Do_While_loop.php
@@ -0,0 +1,8 @@
+<?php 
+
+$num2=1;
+do{
+     echo "5" . " * " .  $num2  ." = " .  5 * $num2 ."<br>";
+    $num2++;
+}while($num2<=12);
+
